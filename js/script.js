7697984f4f33c6716218596289624fe1649a1d5f
Max <maxcamer@gmail.com>
2022-09-21 14:01:28 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+
+
+// const answer = +prompt("Hi, I am max.", "50");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurt.com/${category}/5`);
+
+// const user = "Max";
+
+// alert(`Hello, ${user}`);
+
+// console.log('arr' + " - object");
+// console.log(4 + " - object");
+// console.log(4 + "5");
+
+// let incr = 10,
+//     decr = 10;
+
+    // incr++;
+    // decr--;
+
+    // console.log(++incr);
+    // console.log(--decr);
+
+    // console.log(5%2);
+    // console.log(2*4 == 8);
+
+    const isChecked = false;
+    const isClose = false; 
+
+    console.log(isChecked || !isClose);
