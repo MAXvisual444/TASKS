44e248684cfe8d4c62a0374ea29fcc58ecb30fce
Max <maxcamer@gmail.com>
2022-09-25 15:18:20 -0700

    first lesson

--- js/script.js
+++ js/script.js
@@ -1,43 +1,26 @@
 "use strict";
 
+const numberOfFilms = +prompt('How many movies have you watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false 
+
+};
+
+const a = prompt('One of the last food movies?', ''),
+      b = prompt('How much would you rate it?', ''),
+      c = prompt('One of the last movies I watched?', ''),
+      d = prompt('How much would you rate it?', '');
 
 
-// const answer = +prompt("Hi, I am max.", "50");
-// console.log(answer + 5);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// const answers = [];
+console.log(personalMovieDB);
 
-// answers[0] = prompt('What is your name?', '');
-// answers[1] = prompt('What is your surname?', '');
-// answers[2] = prompt('How old are you?', '');
 
-// console.log(typeof(answers));
 
-// const category = 'toys';
-
-// console.log(`https://someurt.com/${category}/5`);
-
-// const user = "Max";
-
-// alert(`Hello, ${user}`);
-
-// console.log('arr' + " - object");
-// console.log(4 + " - object");
-// console.log(4 + "5");
-
-// let incr = 10,
-//     decr = 10;
-
-    // incr++;
-    // decr--;
-
-    // console.log(++incr);
-    // console.log(--decr);
-
-    // console.log(5%2);
-    // console.log(2*4 == 8);
-
-    const isChecked = false;
-    const isClose = false; 
-
-    console.log(isChecked || !isClose);
