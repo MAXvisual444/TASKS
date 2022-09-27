ec885586d7b18f720e4249f5737954e21bd7c38d
Max <maxcamer@gmail.com>
2022-09-27 09:24:50 -0700

    cycles

--- js/script.js
+++ js/script.js
@@ -1,26 +1,51 @@
 "use strict";
 
-const numberOfFilms = +prompt('How many movies have you watched?', '');
+// const numberOfFilms = +prompt('How many movies have you watched?', '');
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false 
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false 
 
-};
+// };
 
-const a = prompt('One of the last food movies?', ''),
-      b = prompt('How much would you rate it?', ''),
-      c = prompt('One of the last movies I watched?', ''),
-      d = prompt('How much would you rate it?', '');
+// const a = prompt('One of the last food movies?', ''),
+//       b = prompt('How much would you rate it?', ''),
+//       c = prompt('One of the last movies I watched?', ''),
+//       d = prompt('How much would you rate it?', '');
 
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
+let num = 50;
+
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++; 
+// }
+
+for (let i = 1; i < 10; i++) {
+    if (i === 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+}
 
 
