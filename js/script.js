033ecb3f14fc333879c425bb9cbbc0004e8e46ba
Max <maxcamer@gmail.com>
2022-10-04 14:38:12 -0700

    continue

--- js/script.js
+++ js/script.js
@@ -93,3 +93,14 @@
 // }
 
 // console.log(result);
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`); 
+        for (let k = 0; k < 3; k++) {
+            if (k === 5) continue first; // break first;
+            console.log(`Third level: ${k}`); 
+        }
+    }
+}
