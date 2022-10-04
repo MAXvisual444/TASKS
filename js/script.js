b3d055a4266fa85f81991dc4458ace49e5d15923
Max <maxcamer@gmail.com>
2022-10-04 14:18:29 -0700

    cycles while and for label

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@
 
 // console.log(personalMovieDB);
 
-let num = 50;
+// let num = 50;
 
 // while (num <= 55) {
 //     console.log(num);
@@ -40,12 +40,56 @@ let num = 50;
 //     num++; 
 // }
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
 
+// let num = 50;
 
+// while (num <= 55) {
+//     console.log(num);
+//     num++; 
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//     num++;
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j); 
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+//     for (let j = 0; j < i; j++){
+//         result += "*";
+//     }
+
+//     result += '\n';
+// }
+
+// console.log(result);
