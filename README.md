1e2f7ed0c74554636a8e6b73ebd236ef46c544c8
Max <maxcamer@gmail.com>
2022-10-05 20:31:00 -0700

    apply conditions and cycles

--- README.md
+++
@@ -1,2 +0,0 @@
-# project
-start new add project
--- js/script.js
+++ js/script.js
@@ -1,106 +1,34 @@
-"use strict";
+'use strict';
 
-// const numberOfFilms = +prompt('How many movies have you watched?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// const personalMovieDB = {
-//     count: numberOfFilms,
-//     movies: {},
-//     actors: {},
-//     genres: [],
-//     privat: false 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// };
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
 
-// const a = prompt('One of the last food movies?', ''),
-//       b = prompt('How much would you rate it?', ''),
-//       c = prompt('One of the last movies I watched?', ''),
-//       d = prompt('How much would you rate it?', '');
-
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-// console.log(personalMovieDB);
-
-// let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-// for (let i = 1; i < 8; i++) {
-//     console.log(num);
-//     num++; 
-// }
-
-// for (let i = 1; i < 10; i++) {
-//     if (i === 6) {
-//         // break;
-//         continue;
-//     }
-//     console.log(i);
-// }
-
-// let num = 50;
-
-// while (num <= 55) {
-//     console.log(num);
-//     num++; 
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-// for (let i = 1; i < 8; i++) {
-//     console.log(num);
-//     num++;
-// }
-
-// for (let i = 0; i < 3; i++) {
-//     console.log(i);
-//     for (let j = 0; j < 3; j++) {
-//         console.log(j); 
-//     }
-// }
-
-// *
-// **
-// ***
-// ****
-// *****
-// ******
-
-// let result = '';
-// const length = 7;
-
-// for (let i = 1; i < length; i++) {
-
-//     for (let j = 0; j < i; j++){
-//         result += "*";
-//     }
-
-//     result += '\n';
-// }
-
-// console.log(result);
-
-first: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`); 
-        for (let k = 0; k < 3; k++) {
-            if (k === 5) continue first; // break first;
-            console.log(`Third level: ${k}`); 
-        }
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
