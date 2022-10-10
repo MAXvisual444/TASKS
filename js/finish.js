587d22325599b6fc2f5e84695721fb855dc113ed
Maksym <maxcamer@gmail.com>
2022-10-10 14:12:53 -0700

    03-numbers-multiplication

--- js/finish.js
+++ js/finish.js
@@ -1,10 +1,10 @@
-/** Завдання 1 - Оголошення змінної **/
+/** Завдання 1 - Оголошення змінної */
 
 const myCountry = "Ukraine";
 
 console.log(myCountry);
 
-/** Завдання 2 - Привласнення нового значення змінної **/
+/** Завдання 2 - Привласнення нового значення змінної */
 
 let isStudent;
 
@@ -15,3 +15,20 @@ console.log(isStudent);
 isStudent = false;
 
 console.log(isStudent);
+
+/** Завдання 3 - Множення двох чисел */
+
+let a = 2;
+let b = 3;
+
+let c = a * b;
+
+console.log(c); // example A
+
+
+const myNum1 = 4;
+const myNum2 = 7;
+
+const myNum3 = myNum1 * myNum2;
+
+console.log(myNum3); // example B
