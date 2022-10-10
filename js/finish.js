877843875ea3f1506f6ecd6c61a6f0a0080a9ff2
Maksym <maxcamer@gmail.com>
2022-10-10 16:27:40 -0700

    04-strings-concatenation

--- js/finish.js
+++ js/finish.js
@@ -32,3 +32,25 @@ const myNum2 = 7;
 const myNum3 = myNum1 * myNum2;
 
 console.log(myNum3); // example B
+
+/** Завдання 4 - Об'єднання рядків */
+
+const str1 = 'Maksym';
+const str2 = 'Avilov';
+const str3 = 'programmer';
+
+const str4 = str1 + ' ' + str2 + ' ' + str3;
+
+console.log(str4); // option 1
+
+const myName = 'Maksym';
+const mySurname = 'Avilov';
+const myProfession = 'programmer';
+
+const myInfo = 'My name is ' + myName + ' ' + mySurname + ' and I am ' + myProfession;
+
+console.log(myInfo); // option 2
+
+const personInfo = `My name is ${myName} ${mySurname} and I am ${myProfession}`;
+
+console.log(personInfo); // option 3
