9918bdcbbd6380c0c79cb7831a4c5a979aefe9d8
Maksym <maxcamer@gmail.com>
2022-10-25 15:15:57 -0700

    23-division-remaider

--- js/finish.js
+++ js/finish.js
@@ -298,6 +298,19 @@ myVariable2 = 100;
 
 console.log(parseInt(myVariable1) <= parseInt(myVariable2));
 
+// Task 23-division-remainder
 
+const myNumber1 = 10 // 10 = 3 + 3 + 3 + 1
+const myNumber2 = 3
 
+console.log(myNumber1 % myNumber2);
+
+/**
+ * Приоритетность оператора % - 13 (выше чем у операторов + и -)
+ * Ассоциативность оператора % - left-to-right
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+ */
+
+console.log(((100 % 23) % 5) % 3);
 
