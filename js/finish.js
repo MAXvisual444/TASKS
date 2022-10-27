2cf74922cbffdafb67f539faaba971061efb7201
Maksym <maxcamer@gmail.com>
2022-10-27 13:48:36 -0700

    26-expression-or-statement

--- js/finish.js
+++ js/finish.js
@@ -341,3 +341,52 @@ console.log(a); // 17
 // a = a / 2
 a /= 2;
 console.log(a); 
+
+// Task 26-expression-or-statement
+
+/** Определите тип каждой конструкции JavaScript:
+*  - выражение (expression)
+*  - инструкция (statement)
+*  - выражение-инструкция (expression statement)
+*/
+
+// expression statement
+15;
+
+// statement
+const myObject = { // Object literal is expression
+ x: 10,
+ y: true,
+};
+
+// expression statement
+myObject.z = 'abc'; // <-- expression
+
+// expression statement
+delete myObject.x;
+
+// statement
+let newVariable;
+
+// expression statement
+newVariable = 30 + 5; // <-- expression
+
+// expression statement
+console.log(newVariable);
+/**             ^
+*          expression
+*/
+
+// statement
+if (newVariable > 10) {
+ /**       ^
+  *    expression
+  */
+
+ // expression statement
+ console.log(`${newVariable} more 10`); // <-- newVariable is expression
+ /**         _________________________
+  *                      ^
+  *                 expression
+  */
+}
