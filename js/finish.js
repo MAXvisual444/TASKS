e0da3997ceaddcac581db5e23ca2e2b7894058f9
Maksym <maxcamer@gmail.com>
2022-11-03 13:42:11 -0700

    37-Random Number Generator

--- js/finish.js
+++ js/finish.js
@@ -541,4 +541,38 @@ objectKeys.forEach((key) => {
   }
 });
 
+// Task 37-Random Number Generator
+
+const MIN = 1000;
+const MAX = 9999;
+// const MIN = 1;
+// const MAX = 10;
+
+const myNumbers = [2355, 7235, 8135, 1762, 2361, 8351];
+// const myNumbers = [1, 3, 5, 7, 9, 10];
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const addRandomNumberToArray = (arr, min, max) => {
+  let newRandomNumber; 
+  const updatedArr = [ ...arr];
+
+  do {
+    newRandomNumber = randomNumber(min, max);
+    // console.log(newRandomNumber);
+  } while (updatedArr.includes(newRandomNumber));
+
+  updatedArr.push(newRandomNumber);
+
+  return updatedArr;
+};
+
+const updatedArray = addRandomNumberToArray(myNumbers, MIN, MAX);
+
+console.log('UPDATED ARRAY', updatedArray);
+console.log('ORIGINAL ARRAY', myNumbers);
+
+// console.log(myNumbers.includes(2357));
 
