f088db556882232be97133706b6f9314d60576d0
Maksym <maxcamer@gmail.com>
2022-11-18 11:41:53 -0800

    // 45-compare-two-arrays

--- js/finish.js
+++ js/finish.js
@@ -731,3 +731,28 @@ console.log(arraySortInfo(a)); // Some elements are not numbers
 console.log(arraySortInfo(b)); // Sort array in ascending order
 console.log(arraySortInfo(c)); // Sort array in descending order
 console.log(arraySortInfo(d)); // Array is not sorted
+
+// 45-compare-two-arrays
+
+const areArraysEqual = (firstArray, secondArray) => {
+  if (
+    firstArray.length === secondArray.length &&
+    firstArray.every((element, index) => element === secondArray[index]) 
+  ) {
+    return true; 
+  }
+
+  return false;
+};
+
+const a = [1, 2, 3];
+const b = [1, 2, 3]; 
+
+console.log(a === b); // false 
+
+const c = [2, 1, 3];
+const d = [1, 2, 3, 4];
+
+console.log(areArraysEqual(a, b)); // true
+console.log(areArraysEqual(a, c)); // false
+console.log(areArraysEqual(a, d)); // false
