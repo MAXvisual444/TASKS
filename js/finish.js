cb36aadb4d78acda2a14ac1a68a42c7f0505a33d
Maksym <maxcamer@gmail.com>
2022-11-22 14:44:26 -0800

    49-reduce-array-of-objects

--- js/finish.js
+++ js/finish.js
@@ -830,3 +830,39 @@ console.log(myNumbers); // [123, 50, 27, 80]
 
 pushIfUnique(myNumbers, 77);
 console.log(myNumbers); // [123, 50, 27, 80, 77]
+
+// 49-reduce-array-of-objects
+
+function popularPostsIds(posts, minimalComentsQty) {
+  return posts.reduce(
+    (postsIds, post) => 
+      post.comments >= minimalComentsQty
+        ? postsIds.concat([post.postId])
+        : postsIds, 
+    []
+  );
+}
+
+const inputPosts = [
+  {
+    title: 'Как быстро выучить JavaScript?',
+    postId: 3421,
+    comments: 25,
+  },
+  {
+    title: 'Где используется JavaScript?',
+    postId: 5216,
+    comments: 3,
+  },
+  {
+    title: 'Какая разница между React и Angular?',
+    postId: 8135,
+    comments: 12,
+  },
+]
+
+console.log(popularPostsIds(inputPosts, 10)); // [3421, 8135]
+
+console.log(popularPostsIds(inputPosts, 15)); // [3421]
+
+console.log(popularPostsIds(inputPosts, 50)); // []
