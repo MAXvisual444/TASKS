9d328f91ed6c6399be1de059059415f27afba5b8
Maksym <maxcamer@gmail.com>
2022-11-24 18:59:41 -0800

    51-sort-array-of-objects

--- js/finish.js
+++ js/finish.js
@@ -912,3 +912,45 @@ console.log(quantitiesByCategories(inputProducts));
   Phones: 1,
   Watches: 2
 } */
+
+// 51-sort-array-of-objects
+
+const inputProducts = [
+  {
+    title: 'Phone case',
+    price: 23,
+    quantity: 2,
+    category: 'Accessories',
+  },
+  {
+    title: 'Android phone',
+    price: 150,
+    quantity: 1,
+    category: 'Phones',
+  },
+  {
+    title: 'Headphones',
+    price: 78,
+    quantity: 1,
+    category: 'Accessories',
+  },
+  {
+    title: 'Sport Watch',
+    price: 55,
+    quantity: 2,
+    category: 'Watches',
+  },
+];
+
+
+const sortProductsByPrice = (products) =>
+  [...products].sort((a, b) => a.price - b.price)
+
+const sortedProducts = sortProductsByPrice(inputProducts);
+
+console.log(sortedProducts); // Массив отсортированных товаров
+
+console.log(inputProducts); // Оригинальный массив не должен измениться
+
+// const myArray = [50, 23, 10, 29, 108];
+// console.log(myArray.sort((a, b) => b - a));
