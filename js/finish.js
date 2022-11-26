6d0b8180977fc99538c267b00c03983dc54f286a
Maksym <maxcamer@gmail.com>
2022-11-26 12:51:18 -0800

    53-rest-and-spread-operators

--- js/finish.js
+++ js/finish.js
@@ -980,3 +980,40 @@ console.log(templateLiteral(myAnotherNumber));
 Это число больше или равно 10.
 Квадратный корень этого числа - 5.
 */
+
+// 53-rest-and-spread-operators
+
+function meanScore(...numbers) {
+  if (numbers.some((num) => typeof num !== 'number')) { 
+    console.log('Все аргументы в вызове функции должны быть числами!');
+    // throw new Error('Все аргументы в вызове функции должны быть числами!');
+    return;
+  }
+
+  /**
+   * num1 / numbers.length + num2 / numbers.length + ... + num10 / numbers.length
+   * РАВНОСИЛЬНО
+   * (num1 + num2 + ... + num10) / numbers.length
+   */
+
+  return numbers
+    .reduce((mean, num) => mean + num / numbers.length, 0);
+    toFixed(2);
+}
+
+const scores1 = [0, 1.5, 2.5, 3.7];
+const scores2 = [1.7, 4.5, 0, 4.9, 5.0, 4.2];
+const scores3 = [1.3, 2.5, 1.9];
+const scores4 = ['abc', 1.3, true, 2.5, 1.9];
+
+console.log(meanScore(...scores1));
+// 1.93
+
+console.log(meanScore(...scores1, ...scores2));
+// 2.8
+
+console.log(meanScore(...scores1, ...scores2, ...scores3));
+// 2.59
+
+console.log(meanScore(...scores4));
+// Все аргументы в вызове функции должны быть числами!
