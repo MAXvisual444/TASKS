0ab2531d3cc6e9f4d09e8f8f8d162443f2549da3
Maksym <maxcamer@gmail.com>
2022-11-27 20:40:26 -0800

    54-enhanced-odject-literals

--- js/finish.js
+++ js/finish.js
@@ -1043,3 +1043,42 @@ console.log(weatherForecast('Miami', ''));
 
 console.log(weatherForecast('Las Vegas', undefined));
 // Прогноз погоды для города Las Vegas: Отличная погода!
+
+// 54-enhanced-odject-literals
+
+const photosGallery = (title, dimensions, date) => {
+  return {
+    title,
+    date,
+    [dimensions]: true,
+    info() {
+      console.log(`Фото "${title}" имеет разрешение ${dimensions}`);
+    },
+    publishInfo() {
+      console.log(
+        `Фото "${title}" было опубликовано ${Math.floor(
+          (new Date().getTime() - date.getTime()) / 1000
+        )} секунды назад`
+      );
+    },
+  };
+};
+
+const myDogPhoto = photosGallery('My dog', '1920x1080', new Date());
+
+const testDimension1 = '1920x1080';
+const testDimension2 = '1080x720';
+
+myDogPhoto.info();
+/* Фото "My dog" имеет разрешение 1920x1080 */
+
+setTimeout(() => myDogPhoto.publishInfo(), 2000);
+/* Фото "My dog" было опубликовано 2 секунды назад */
+
+/* ВОПРОС: Почему метод "publishInfo" все еще имеет доступ 
+к параметрам функции "photosGallery" (например "date")? */
+
+console.log(myDogPhoto[testDimension1]); // true
+console.log(myDogPhoto[testDimension2]); // undefined
+
+// console.log(Object.keys(myDogPhoto))
