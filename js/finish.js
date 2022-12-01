97a1121d26a0195459638fbb71add18bc94de711
Maksym <maxcamer@gmail.com>
2022-12-01 12:46:55 -0800

    55-array-destructuring

--- js/finish.js
+++ js/finish.js
@@ -1082,3 +1082,18 @@ console.log(myDogPhoto[testDimension1]); // true
 console.log(myDogPhoto[testDimension2]); // undefined
 
 // console.log(Object.keys(myDogPhoto))
+
+// 55-array-destructuring
+
+const processQuantities = ([minQty, maxQty, defaultQty = 0]) => {
+  console.log(minQty) // 8
+  console.log(maxQty) // 29
+  console.log(defaultQty) // 10 в первом вызове, 0 во втором вызове
+  return defaultQty + maxQty - minQty
+}
+
+const inputQuantities1 = [8, 29, 10]
+console.log(processQuantities(inputQuantities1)) // 31
+
+const inputQuantities2 = [8, 29]
+console.log(processQuantities(inputQuantities2)) // 21
