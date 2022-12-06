45b7e55072d005b0c60ac35a0d25d740073ca194
Maksym <maxcamer@gmail.com>
2022-12-06 13:55:07 -0800

    59-delete-object-properties

--- js/finish.js
+++ js/finish.js
@@ -1169,7 +1169,6 @@ const processPosts = (posts) => {
       commentsQty: postCommentsQty = 0,
     } = post;
 
-
     return {
       postId: postId +1000,
       postAuthor,
@@ -1177,7 +1176,6 @@ const processPosts = (posts) => {
     };
   });
 };
-
 
 const testPosts = [
   {
@@ -1194,7 +1192,7 @@ const testPosts = [
     author: 'merryl',
     commentsQty: 8,
   },
-]
+];
 
 const processedPosts = processPosts(testPosts);
 console.log(processedPosts);
@@ -1219,3 +1217,41 @@ console.log(processedPosts);
 */
 
 console.log(testPosts);
+
+// 59-delete-object-properties
+
+let person = {
+  _id: '5ad8cefcc0971792dacb3f1f',
+  index: 4,
+  processed: false,
+  cart: ['item1', 'item2', 'item3'],
+  email: 'slarsen@test.com',
+  name: 'Samanta Larsen',
+  cartId: 435,
+};
+
+{
+  let _id, processed, cart
+  ;({ _id, processed, cart, ...person } = person);
+  console.log(_id, processed, cart);
+}
+
+// // OPTION: Delete operator
+// delete person._id
+// delete person.processed
+// delete person.cart
+
+// // ReferenceError: _id is not defined (Because _id, processed and cart are block-scoped variables)
+// console.log(_id, processed, cart)
+
+console.log(person);
+/*
+{
+  index: 4,
+  email: "slarsen@test.com",
+  name: "Samanta Larsen",
+  cartId: 435
+}
+*/
+
+
