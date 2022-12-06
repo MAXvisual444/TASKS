469bfbc761be6a49e2e7399907e618351fe78cf0
Maksym <maxcamer@gmail.com>
2022-12-06 13:31:50 -0800

    58-array-transformation

--- js/finish.js
+++ js/finish.js
@@ -1158,3 +1158,64 @@ console.log(result)
   createdAtYear: *current year*
 }
 */
+
+// 58-array-transformation
+
+const processPosts = (posts) => {
+  return posts.map((post) => {
+    const {
+      postId,
+      author: postAuthor,
+      commentsQty: postCommentsQty = 0,
+    } = post;
+
+
+    return {
+      postId: postId +1000,
+      postAuthor,
+      postCommentsQty,
+    };
+  });
+};
+
+
+const testPosts = [
+  {
+    postId: 234,
+    author: 'robd',
+    commentsQty: 5,
+  },
+  {
+    postId: 823,
+    author: 'sady',
+  },
+  {
+    postId: 161,
+    author: 'merryl',
+    commentsQty: 8,
+  },
+]
+
+const processedPosts = processPosts(testPosts);
+console.log(processedPosts);
+/*
+[
+  {
+    postId: 1234,
+    postAuthor: 'robd',
+    postCommentsQty: 5
+  },
+  {
+    postId: 1823,
+    postAuthor: 'sady',
+    postCommentsQty: 0
+  },
+  {
+    postId: 1161,
+    postAuthor: 'merryl',
+    postCommentsQty: 8
+  }
+]
+*/
+
+console.log(testPosts);
