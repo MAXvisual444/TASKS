231bdad948085c8d117995ef311fd7b40b9a1f51
Maksym <maxcamer@gmail.com>
2022-12-09 16:11:57 -0800

    62-sum-number

--- js/finish.js
+++ js/finish.js
@@ -1296,3 +1296,25 @@ console.log(myCustomArray);
 myCustomArray.Push(30);
 
 console.log(myCustomArray);
+
+// 62-sum-number
+
+function sumNumbers() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+    
+  }
+  console.log('Sum of all arguments is ', sum);
+
+  return sum;
+}
+
+sumNumbers(1, 3);
+// 4
+
+sumNumbers(10, 20, 5);
+// 35
+
+sumNumbers(2, 5, 80, 1, 10, 12);
+// 110
