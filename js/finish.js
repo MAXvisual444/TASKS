3d1dd0840bd3bdc15f9362e620ee9b4d5f0144c9
Maksym <maxcamer@gmail.com>
2023-01-20 22:59:47 -0800

    72 - Spread Operator

--- js/finish.js
+++ js/finish.js
@@ -1500,3 +1500,21 @@ console.log(b)
 
 console.log(c)
 // [3, 4, 5, 6, 7]
+
+
+// 72 - Spread Operator
+
+const a = [1, 2];
+const b = [4, 5];
+const c = [8, 9, 10];
+const d = 11;
+
+const combinedArray = [0, ...a, 3, ...b, 6, 7, ...c, d];
+
+console.log(combinedArray);
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
